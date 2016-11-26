65851777462ee42afc8d771bf17c4efd2c52b197
Giorgi shesheli <giorgishesheli@gmail.com>
2016-11-26 12:26:37 +0400

    Some cleanup, before making board size dynamic

--- main.c
+++ main.c
@@ -6,22 +6,23 @@
 
 #include <ncurses.h>
 
-int wind_cursor[2];
-int log_cursor[2] = {0, 0};
-int board[3][3] = {
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0}
-};
-
-int turn = 1; // 1 X's turn, 0 means it's O's turn
-int turn_num = 0;
+#ifndef BOARD_SIZE
+#define BOARD_SIZE 3
+#endif
 
 #ifdef DEBUG
 #define dbg(...)  fprintf(stderr, __VA_ARGS__)
 #else
 #define dbg(...)
 #endif
+
+int cursor_x, cursor_y = 0; // logical cursor location
+int board_x, board_y;  // Board location
+
+int board[BOARD_SIZE][BOARD_SIZE];
+
+int turn_num = 0; 
+
 
 
 void draw_board(int init_x, int init_y){
@@ -69,22 +70,17 @@ int sum(int *array){
 }
 
 void win_or_draw(){
-	/*
-	dbg(" {0, 0} = %d, {0, 1} = %d, {0, 2} = %d, {1,0} = %d, {1, 1} = %d, {1, 2} = %d, {2,0} = %d, {2,1} = %d, {2,2} = %d\n",
-			board[0][0], board[0][1], board[0][2],
-			board[1][0], board[1][1], board[1][2],
-			board[2][0], board[2][1], board[2][2] );
-			*/
-	if(turn_num == 9){
-		dbg("Draw \n");
-	}
 
-	int hor_array[] = { board[0][log_cursor[1]], board[1][log_cursor[1]], board[2][log_cursor[1]] };
-	if(sum(board[log_cursor[0]]) == 12 || sum(hor_array) == 12){
+	int hor_array[] = { board[0][cursor_y], board[1][cursor_y], board[2][cursor_y] };
+	if(sum(board[cursor_x]) == 12 || sum(hor_array) == 12){
 		dbg("O Won\n");
 	}
-	if(sum(board[log_cursor[0]]) == 3 || sum(hor_array) == 3){
+	if(sum(board[cursor_x]) == 3 || sum(hor_array) == 3){
 		dbg("X Won \n");
+	}
+
+	if(turn_num == 9){
+		dbg("Draw \n");
 	}
 
 }
@@ -92,64 +88,57 @@ void win_or_draw(){
 
 void loop(){
 	char key;
-
 	for(;;){
 	key = getch();
 		switch(key){
 			case 'j':
-				move(wind_cursor[1] + 2, wind_cursor[0]);
-				refresh();
-				wind_cursor[1] += 2;
-				log_cursor[1]++;
+				cursor_y += 1;
 				break;
 			case 'k':
-				move(wind_cursor[1] - 2, wind_cursor[0]);
-				refresh();
-				wind_cursor[1] -= 2;
-				log_cursor[1]--;
+				cursor_y -= 1;
 				break;
 			case 'h':
-				move(wind_cursor[1], wind_cursor[0] - 4);
-				refresh();
-				wind_cursor[0] -= 4;
-				log_cursor[0]--;
-				break;
+				cursor_x -= 1;
 				break;
 			case 'l':
-				move(wind_cursor[1], wind_cursor[0] + 4);
-				refresh();
-				wind_cursor[0] += 4;
-				log_cursor[0]++;
+				cursor_x += 1;
 				break;
 			case ' ':
-				if(turn){
+				if(turn_num % 2 == 0){
 					addch('X');
-					board[log_cursor[0]][log_cursor[1]] = 1;				
-					turn = 0;
+					board[cursor_x][cursor_y] = 1;				
 				} else {
 					addch('O');
-					board[log_cursor[0]][log_cursor[1]] = 4;				
-					turn = 1;
+					board[cursor_x][cursor_y] = 4;				
 				}
-				move(wind_cursor[1], wind_cursor[0]);
+
 				turn_num++;
 				win_or_draw();
-				dbg("log_cursor[0] = %d, log_cursor[1] = %d\n", log_cursor[0], log_cursor[1]);
-				refresh();
-				break;
-			default:
-				break;
+		}
+
+		move(board_y + cursor_y * 2, board_x + cursor_x * 4 );
+		refresh();
+	}
+}
+
+void zero_board(){
+	int i, j;
+	for(i = 0; i < BOARD_SIZE; i++){
+		for(j = 0; j < BOARD_SIZE; j++){
+			board[i][j] = 0;
 		}
 	}
 }
+	       			       
 
 void start_game(){
 	int x = (COLS / 2) - 8;
 	int y = (int) LINES * 0.2;
 	draw_board(x,y);
-	wind_cursor[0] = x + 1;
-	wind_cursor[1] = y;
-	wmove(stdscr, wind_cursor[1], wind_cursor[0]);
+	board_x = x + 1;
+	board_y = y;
+	zero_board(); // Safely zero out board array
+	wmove(stdscr, board_y, board_x);
 	refresh();
 	loop();
 }
@@ -159,15 +148,7 @@ int main(int argc, char **argv){
 	setlocale(LC_ALL, "");
 	initscr();
 	cbreak();
-	//curs_set(0);
 	noecho();
 	start_game();
-	//mvaddch(11, 12, ACS_VLINE);
-	//mvaddch(12, 11, ACS_HLINE);
-
-
-
-	pause();
-
 	exit(EXIT_SUCCESS);
 }
